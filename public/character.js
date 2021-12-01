84332835756ad6ef77fbaaef575aa237428d4a84
jaredblack <jrdblck@gmail.com>
2021-11-30 20:36:00 -0700

    Implement basic character generation page functionality

---
+++ public/character.js
@@ -0,0 +1,370 @@
+let characterList = [
+    {
+        sourceText: "",
+        name: "",
+        nameSyllables: [
+
+        ],
+        goodTraits: [
+
+        ],
+        badTraits: [
+
+        ]
+    },
+    {
+        sourceText: "Ramayana",
+        name: "Rama",
+        nameSyllables: [
+            "Ra",
+            "ma"
+        ],
+        goodTraits: [
+            "Virtuous, strong, just",
+            "Loved their wife"
+        ],
+        badTraits: [
+            "Appeared not to really trust their spouse and subjected them to tests"
+        ]
+    },
+    {
+        sourceText: "Ramayana",
+        name: "Ravana",
+        nameSyllables: [
+            "Ra",
+            "va",
+            "na"
+        ],
+        goodTraits: [
+            "A learned scholar who is versed in scripture"
+        ],
+        badTraits: [
+            "Abducted an innocent woman",
+            "Often seen as evil incarnate"
+        ]
+    },
+    {
+        sourceText: "Antigone",
+        name: "Antigone",
+        nameSyllables: [
+            "An",
+            "ti",
+            "go",
+            "ne"
+        ],
+        goodTraits: [
+            "Fiercely loyal to their family",
+            "Devoted to the divine rule over the earthly"
+        ],
+        badTraits: [
+            "Stubbornness becomes their downfall"
+        ]
+    },
+    {
+        sourceText: "Antigone",
+        name: "Creon",
+        nameSyllables: [
+            "Cre",
+            "on"
+        ],
+        goodTraits: [
+            "Devoted to logic and the rule of law",
+            "Only has the best of intentions"
+        ],
+        badTraits: [
+            "A monarch turned tyrannical",
+            "Lets logic and earthly law blind their better judgment and mercy",
+            "Eventually changes their mind to what's right, but it's too late"
+        ]
+    },
+    {
+        sourceText: "Antigone",
+        name: "Teiresias",
+        nameSyllables: [
+            "Tei", "res", "ias"
+        ],
+        goodTraits: [
+            "Loyal to divine prophecy",
+            "Receives visions of the future"
+        ],
+        badTraits: [
+            "Blind",
+            "Nobody believes them"
+        ]
+    },
+    {
+        sourceText: "Antigone",
+        name: "Ismene",
+        nameSyllables: [
+            "Is", "me", "ne"
+        ],
+        goodTraits: [
+            "Values family bonds, the divine law over the laws of man"
+        ],
+        badTraits: [
+            "Only loyal to family when it doesn't put their life in danger",
+            "Doesn't stand up for their beliefs when it comes time"
+        ]
+    },
+    {
+        sourceText: "Song of Lasting Regret",
+        name: "Yang Guifei",
+        nameSyllables: [
+            "Yang", "gui", "fei"
+        ],
+        goodTraits: [
+            "Loved lychee"
+        ],
+        badTraits: [
+
+        ]
+    },
+    {
+        sourceText: "Song of Lasting Regret",
+        name: "Emperor Tang Xuanzong",
+        nameSyllables: [
+            "Emp", "er", "or", "xuan", "zong"
+        ],
+        goodTraits: [
+            "Brought their dynasty to its apex in what was referred to as the \"glorious era\""
+        ],
+        badTraits: [
+            "Ignored administrative duties as they focused more on love"
+        ]
+    },
+    {
+        sourceText: "Bisclavret",
+        name: "Bisclavret",
+        nameSyllables: [
+            "Bis", "clav", "ret"
+        ],
+        goodTraits: [
+            "Well-loved by the king"
+        ],
+        badTraits: [
+            "Doesn't tell their spouse where they go for three days of the week",
+            "Turns into a werewolf every week"
+        ]
+    },
+    {
+        sourceText: "Bisclavret",
+        name: "Wife",
+        nameSyllables: [
+            "Wife"
+        ],
+        goodTraits: [
+
+        ],
+        badTraits: [
+            "Plots big revenge on their spouse over something they can't control"
+        ]
+    },
+    {
+        sourceText: "Laustic",
+        name: "Nightingale",
+        nameSyllables: [
+            "Night", "in", "gale"
+        ],
+        goodTraits: [
+            "Beautiful",
+            "Symbolizes love"
+        ],
+        badTraits: [
+            "Can't talk",
+            "Will get brutally killed"
+        ]
+    },
+    {
+        sourceText: "Laustic",
+        name: "Husband",
+        nameSyllables: [
+            "Hus", "band"
+        ],
+        goodTraits: [
+
+        ],
+        badTraits: [
+            "Their spouse is in love with the neighbor"
+        ]
+    },
+    {
+        sourceText: "Decameron",
+        name: "Gualtieri",
+        nameSyllables: [
+            "Gual", "ti", "eri"
+        ],
+        goodTraits: [
+
+        ],
+        badTraits: [
+            "Treats their spouse horribly as part of a series of \"tests\"",
+            "Sends their child away, leads spouse to believe said child is dead"
+        ]
+    },
+    {
+        sourceText: "Decameron",
+        name: "Griselda",
+        nameSyllables: [
+            "Gri", "sel", "da"
+        ],
+        goodTraits: [
+            "Perfectly and exactly obedient",
+            "A great shepherd"
+        ],
+        badTraits: [
+
+        ]
+    },
+    {
+        sourceText: "The Travels of Sir John Mandeville",
+        name: "John Mandeville",
+        nameSyllables: [
+            "John", "Man", "de", "ville"
+        ],
+        goodTraits: [
+            "A hugely popular world traveler"
+        ],
+        badTraits: [
+            "Has a terrible tendency to exaggerate and make things up"
+        ]
+    },
+    {
+        sourceText: "Divine Comedy",
+        name: "Dante",
+        nameSyllables: [
+            "Dan", "te"
+        ],
+        goodTraits: [
+            "Empathetic and compassionate"
+        ],
+        badTraits: [
+            "Weak constitution, has a tendency to pass out",
+            "Doesn't always have the best of judgment",
+            "Can become harsh when under pressure"
+        ]
+    },
+    {
+        sourceText: "Divine Comedy",
+        name: "Virgil",
+        nameSyllables: [
+            "Vir", "gil"
+        ],
+        goodTraits: [
+            "A great writer",
+            "A guide in many aspects: a tutor, a father figure, a schoolmaster, a literal guide through the darkest and coldest moments"
+        ],
+        badTraits: [
+            "Can get a little stern at times"
+        ]
+    },
+    {
+        sourceText: "Don Quixote",
+        name: "Don Quixote",
+        nameSyllables: [
+            "Don", "qui", "xote"
+        ],
+        goodTraits: [
+            "Wealthy",
+            "Smart and well-read"
+        ],
+        badTraits: [
+            "Has a lot of trouble telling reality from their imagination",
+            "Hurts people who have not done anything wrong",
+            "Has never actually met the lover that they claim to be doing everything for"
+        ]
+    },
+    {
+        sourceText: "Don Quixote",
+        name: "Sancho Panza",
+        nameSyllables: [
+            "San", "cho", "pan", "za"
+        ],
+        goodTraits: [
+            "Loyal to their master",
+            "Sneakily wise"
+        ],
+        badTraits: [
+            "Really only doing what they're doing for personal gain",
+            "Not portrayed as being very bright"
+        ]
+    },
+]
+
+function showDiv() {
+    let div = document.getElementById("characterBox")
+    div.style.display = "block"
+    newCharacter = getNewCharacter()
+    document.getElementById("name").innerText = newCharacter.name
+    document.getElementById("goodTrait1").innerText = newCharacter.goodTraits[0].goodTrait
+    document.getElementById("goodTrait1").title = `Trait from ${newCharacter.goodTraits[0].characterName} from ${newCharacter.goodTraits[0].sourceText}`
+    document.getElementById("goodTrait2").innerText = newCharacter.goodTraits[1].goodTrait
+    document.getElementById("goodTrait2").title = `Trait from ${newCharacter.goodTraits[1].characterName} from ${newCharacter.goodTraits[1].sourceText}`
+    document.getElementById("badTrait1").innerText = newCharacter.badTraits[0].badTrait
+    document.getElementById("badTrait1").title = `Trait from ${newCharacter.badTraits[0].characterName} from ${newCharacter.badTraits[0].sourceText}`
+    document.getElementById("badTrait2").innerText = newCharacter.badTraits[1].badTrait
+    document.getElementById("badTrait2").title = `Trait from ${newCharacter.badTraits[1].characterName} from ${newCharacter.badTraits[1].sourceText}`
+}
+
+function getNewCharacter() {
+    let goodTraits = [
+        allGoodTraits[Math.floor(Math.random() * allGoodTraits.length)],
+        allGoodTraits[Math.floor(Math.random() * allGoodTraits.length)]
+    ]
+    let badTraits = [
+        allBadTraits[Math.floor(Math.random() * allBadTraits.length)],
+        allBadTraits[Math.floor(Math.random() * allBadTraits.length)]
+    ]
+    let name = createName(goodTraits, badTraits)
+
+    return {
+        name: name,
+        goodTraits: goodTraits,
+        badTraits: badTraits
+    }
+}
+
+function createName(goodTraits, badTraits) {
+    allSyllables = []
+    for (let trait of goodTraits) {
+        allSyllables.push(trait.nameSyllables)
+    }
+    for (let trait of badTraits) {
+        allSyllables.push(trait.nameSyllables)
+    }
+    allSyllables.sort(function (a, b) {
+        return a.length - b.length;
+    })
+    let name = ""
+    for (let i = 0; i < allSyllables.length; i++) {
+        if (allSyllables[i].length > i) {
+            name += allSyllables[i][i]
+        } else {
+            break
+        }
+    }
+    return name
+}
+
+
+let allGoodTraits = []
+let allBadTraits = []
+for (let character of characterList) {
+    for (let goodTrait of character.goodTraits) {
+        trait = {
+            characterName: character.name,
+            sourceText: character.sourceText,
+            nameSyllables: character.nameSyllables,
+            goodTrait: goodTrait
+        }
+        allGoodTraits.push(trait)
+    }
+    for (let badTrait of character.badTraits) {
+        trait = {
+            characterName: character.name,
+            sourceText: character.sourceText,
+            nameSyllables: character.nameSyllables,
+            badTrait: badTrait
+        }
+        allBadTraits.push(trait)
+    }
+}
